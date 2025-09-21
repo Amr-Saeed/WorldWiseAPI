174acf5d535955bab185c5da28cb17e47d088bfc
Amr <amr.saeed5.0a@gmail.com>
2025-09-21 23:24:33 +0300

    renaming from cities to db for deploying

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-// server.js
 import jsonServer from "json-server";
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults({ noCors: false });
+const middlewares = jsonServer.defaults();
 
 server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // allow all origins
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
     next();
 });
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(3000, () => {
-    console.log("JSON Server is running");
-});
+export default server;
